edc70599c03eecc08f73861c9b679ce18d23772a
JAFFAR JAWED <jaffarjawed@users.noreply.github.com>
2019-10-21 23:46:19 +0530

    Update Sinlge_Linked_List_Operations.c

--- Sinlge_Linked_List_Operations.c
+++ Sinlge_Linked_List_Operations.c
@@ -2,131 +2,121 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 struct node
 {
     int data;
     struct node* link;
 };
-struct node* root = NULL;
 int len;
-
-void append(void);
-void addatbegin(void);
-void addafter(void);
+struct node *root = NULL;
+void insert();
+void addatbegin();
 int length();
-void display();
+void print();
+void reverseIterative();
+void printReverseRecursive();
+void reverseRecursive();
 void delete();
+void insertAnyPos();
 void swap();
-void reverse_linked_list();
-
 void main()
 {
     int ch;
-    while(1)
+    len = length();
+    while (1)
     {
-        printf("1.Append\n");
-        printf("2.AddAtBegin\n");
-        printf("3.AddAfter\n");
-        printf("4.length\n");
-        printf("5.Display Items\n");
-        printf("6.Delete At Any Location\n");
-        printf("7.Swapping of nodes\n");
-        printf("8.Reverse of Linked List\n");
-        printf("9.Quit\n\n");
-        printf("Enter the choice: ");
-        scanf("%d",&ch);
+        printf("1.Insert at Last\n");
+        printf("2.Insert at Begining\n");
+        printf("3.Length of Linked List\n");
+        printf("4.Print Element in Single Linked List\n");
+        printf("5.Reverse Element in Linked List using iterative\n");
+        printf("6.Print Element in Reverse Order Using Recursive\n");
+        printf("7.Reverse element by Using Recursive Process\n");
+        printf("8.Delete Elements from the Linked List\n");
+        printf("9.Add nodes at any Postion\n");
+        printf("10.Swap two adjacent nodes in Single Linked List\n");
+        printf("11.Quit\n");
+        printf("Enter your choice:");
+        scanf("%d", &ch);
+        printf("\n");
 
-        switch(ch)
+
+        switch (ch)
         {
-            case 1: append();
-                    break;
-            case 2: addatbegin();
-                    break;
-            case 3: addafter();
-                    break;
-            case 4: len = length();
-                    printf("%d is length of linked list\n",len);
-                    printf("\n");
-                    break;
-            case 5: display();
-                    break;
-            case 6: delete();
-                    break;
-            case 7: swap();
-                    break;
-            case 8: reverse_linked_list();
-                    break;
-            case 9: exit(1);
-            default: printf("Invalid Choice");
+        case 1: insert();
+            break;
+        
+        case 2: addatbegin();
+            break;
+        
+        case 3: len = length();
+                printf("Linked List size is %d\n\n", len);
+            break;
+
+        case 4: print();
+            break;
+
+        case 5: reverseIterative();
+            break;
+
+        case 6: printReverseRecursive(root);
+            break;
+
+        case 7: reverseRecursive(root);
+            break;
+
+        case 8: delete();
+            break;
+
+        case 9: insertAnyPos();
+            break;
+
+        case 10: swap();
+            break;
+
+        case 11: exit(0);
+            break;
+        default: printf("Invalid Choice\n");
+            break;
         }
-
-
     }
+    
+
 }
-// Appending elements in Single Linked List
-void append()
+
+void insert()
 {
     struct node* temp;
     struct node* p;
+    p = root;
     temp = (struct node*)malloc(sizeof(struct node));
-    printf("Enter you data: ");
+    printf("Enter your data:");
     scanf("%d", &temp ->data);
-    printf("\n");
-    temp -> link = NULL;
+    temp ->link = NULL;
     if (root == NULL)
     {
         root = temp;
     }
     else
     {
-        p = root;
-        while(p ->link != NULL)
+        while(p->link != NULL)
         {
             p = p ->link;
-
         }
-        p -> link = temp;
+        p ->link = temp;
     }
+    
 
 }
-
-//Adding elements at beginning of linked list
 void addatbegin()
 {
+    int i= 1;
     struct node* temp;
     temp = (struct node*)malloc(sizeof(struct node));
-    printf("Enter Your data: ");
+    printf("Enter your data:");
     scanf("%d", &temp ->data);
-    if(root == NULL)
-    {
-        root = temp;
-    }
-    else
-    {
-        temp ->link = root;
-        root = temp;
-    }
-
-}
-
-//Adding elements after specified position in Single Linked List
-void addafter()
-{
-    struct node* temp;
-    struct node* p;
-    int loc,i = 1;
-    temp = (struct node*)malloc(sizeof(struct node));
-    printf("Enter your location:");
-    scanf("%d", &loc);
-    len = length();
-    if(loc > len)
-    {
-        printf("Invalid Location\n");
-    }
-    else if(loc == 1)
-    {
-        printf("Enter your data:");
-        scanf("%d",&temp ->data);
+    temp ->link = NULL;
         if (root == NULL)
         {
             root = temp;
@@ -135,83 +125,105 @@ void addafter()
         {
             temp ->link = root;
             root = temp;
-        }
-    }
-    else
-    {
-        p = root;
-        while(i <loc )
-        {
-            p = p ->link;
-            i++;
-        }
-        printf("Enter your data:");
-        scanf("%d", &temp ->data);
-        temp ->link = NULL;
-        temp ->link = p ->link;
-        p ->link = temp;
-    }
-
+        }  
 }
-
-//Finding the length of Single Linked List
 int length()
 {
-    struct node* temp;
+    struct node *temp;
     temp = root;
     int count = 0;
-    while(temp != NULL)
+    while (temp != NULL)
     {
         count++;
-        temp = temp -> link;
+       temp = temp ->link;
     }
+    printf("\n");
     return count;
 }
-//Displaying elements of Single Linked List
-void display()
+void print()
 {
-    struct node* temp;
-    if(root == NULL)
+    struct node *temp;
+    temp = root;
+    if (root == NULL)
     {
-        printf("Linked List is Empty\n");
+        printf("List is Empty\n");
     }
     else
     {
-        temp = root;
         while(temp != NULL)
         {
-            printf("%d -->", temp -> data);
-            temp = temp -> link;
+            printf("%d ->", temp ->data);
+            temp = temp ->link;
         }
-        printf("\n\n");
+        printf("\n");
     }
+}
+void reverseIterative()
+{
+    struct  node* p, *q;
+    p = q = root;
+    int k, temp,  i = 0;
+    int j = length() -1;
+    while(i < j)
+    {
+        k = 0;
+        while(k <j)
+        {
+            q = q ->link;
+            k++;
+        }
+        temp = p ->data;
+        p ->data = q ->data;
+        q ->data = temp;
+        i++;
+        j--;
+        p = p ->link;
+        q = root;
+    }
+}
+void printReverseRecursive(struct node* temp)
+{
+    
+    if (temp == NULL)
+        return;
+    printReverseRecursive(temp ->link);
+    printf("%d ->", temp ->data);
+    printf("\n");
+}
+void reverseRecursive(struct node* p)
+{
+    if(p ->link == NULL)
+    {
+        root = p;
+        return;
+    }
+    reverseRecursive(p->link);
+   p->link->link = p;
+   p ->link = NULL;
 
 }
-
-//Deleting any elements in Single Linked List at Specified Position
 void delete()
 {
-    struct node* temp;
-    int loc;
-    len = length();
+    int loc, i = 1;
+    int len = length();
     printf("Enter your location to delete:");
     scanf("%d", &loc);
-    if (loc > len)
+    if(loc > len)
     {
-        printf("Invalid Location\n");
+        printf("Invalid Location\n\n");
+
     }
     else if(loc == 1)
     {
-        temp = root;
-        root = temp ->link;
-        free(temp);
+            struct node* temp = root;
+            root = temp ->link;
+            temp ->link = NULL;
+            free(temp);
     }
     else
     {
-        struct node* p;
+        struct node *p = root;
         struct node* q;
-        p = root;
-        int i = 1;
         while(i < loc-1)
         {
             p = p ->link;
@@ -222,49 +234,87 @@ void delete()
         q ->link = NULL;
         free(q);
     }
+    
 }
-//Swapping elements of Single Linked List at Specified Position
+
+void insertAnyPos()
+{
+    int loc, i = 1;
+    len = length();
+    struct node* temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp ->link = NULL;
+    printf("Enter your location to append elements:");
+    scanf("%d", &loc);
+    if( loc > len)
+    {
+        printf("Invalid location");
+        printf("List is having %d elements", len);
+    }
+    else if (loc ==1 )
+    {
+        printf("Enter you data:");
+        scanf("%d", &temp ->data);
+        if (root == NULL)
+        {
+            temp = root;
+        }
+        else
+        {
+            temp ->link = root;
+            root = temp;
+        }
+        
+    }
+    else
+    {
+        struct node *p = root;
+        printf("Enter your data:");
+        scanf("%d", &temp ->data);
+        while(i < loc)
+        {
+            p = p ->link;
+            i++; 
+        }
+        temp ->link = p ->link;
+        p ->link = temp;
+    }
+}
 void swap()
 {
-    struct node* p, *q, *r;
     int loc, i = 1;
+    struct node* r , *p, *q, *temp;
     p = root;
-    printf("Enter your location to swap:");
+    temp = root;
+    printf("Enter loccation to swap:");
     scanf("%d", &loc);
-    while(i < loc -1)
+    if (loc > len)
     {
-        p = p ->link;
-        i++;
+        printf("Invalid Location\n");
+        printf("List is having %d elements\n\n", len);
     }
+    else if(loc == 1)
+    {
+        q = p ->link;
+        r = q ->link;
+        p ->link = r;
+        q ->link = temp;
+        root = q;
+
+
+    }
+    else
+    {
+        while (i < loc -1)
+        {
+            p = p->link;
+            i++;
+        }
     q = p ->link;
     r = q ->link;
     q ->link = r ->link;
     r ->link = q;
-    p -> link = r;
-    printf("Swapped Successfully\n");
-}
-//REverse the Elements of Single Linked List
-void reverse_linked_list()
-{
-    struct node* p, *q;
-    p = q =root;
-    len = length();
-    int i, j,k, temp;
-    i = k = 0;
-    j = len -1;
-    while(i < j)
-    {
-        k= 0;
-        while(k < j)
-        {
-            q = q ->link;
-            k++;
-        }
-    temp = p ->data;
-    p ->data = q ->data;
-    q -> data = temp;
-    p = p -> link;
-    q = root;
-    i++, j--;
+    p ->link = r;
     }
 }
+    
